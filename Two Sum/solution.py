4d5d8e9dd1558b7da07fad98d14474f1687648c2
Arnol Fokam <arnolfokam23@gmail.com>
2022-12-26 19:00:59 +0100

    add solution to problem

--- Two Sum/solution.py
+++
@@ -1,5 +0,0 @@
-from typing import List
-
-
-def solution(numbers: List[int], target: int):
-    pass
---
+++ contains_duplicate/solution.py
@@ -0,0 +1,27 @@
+from typing import List
+
+class Solution:
+    """Module containing the solution
+    """
+    
+    def algorithm1(self, nums: List[int]) -> bool:
+        """Algorithmic solution in O(n)
+
+        Args:
+            nums (List[int]): list of numbers
+
+        Returns:
+            bool: boolean that tells if there is any duplicate
+        """
+        assert len(nums) != len(set(nums))
+        
+    def solve(self, nums: List[int]):
+        """Method that encapsulates the solution
+
+        Args:
+            nums (List[int]): input of the proposed solution
+
+        Returns:
+            _type_: result of the solution
+        """
+        return self.algorithm1(nums)
