a397bf61f1f5a9cf194ce7f94c54d35019290705
Arnol Fokam <arnolfokam23@gmail.com>
2023-03-01 19:02:54 +0200

    add problem

---
+++ best_time_to_buy_and_sell_stock/solution.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class Solution:
+    """Module containing the solution
+    """
+        
+    def solve(self, prices: List[int]) -> int:
+        """ calculates the max profit from buying and selling stock
+
+        Args:
+            prices (List[int]): list of prices
+
+        Returns:
+            int: max profit
+        """
+        
